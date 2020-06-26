348cfdc569ad252dd813494ebc01f023edeb765d
Wilber Jurado <wilberamp@gmail.com>
2020-06-26 00:14:53 -0500

    Primero

---
+++ PROYECTO.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+	<h1>Hola mundo</h1>
+	<p>Cascada de mierda pura</p>
+<?php
+	
+echo("Hola mundo php");
+
+	
+?>
+
+<body>
+
+<div class="mia" id="prueba">Colocar aquí el contenido para  class "mia" id "prueba"</div>
+</body>
+</html>
