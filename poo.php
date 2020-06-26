09ecccd134a27cd20f2e2f1dc8669a1d11842184
Wilber Jurado <wilberamp@gmail.com>
2020-06-26 00:25:09 -0500

    Creación del archivo POO

---
+++ poo.php
@@ -0,0 +1,40 @@
+<?php
+
+class Coche {
+	
+	var $ruedas;
+	var $color;
+	var $Motor;
+		
+	function Coche (){
+		$this->ruedas=4;
+		$this->color="Rojo";
+		$this->Motor="1600cc";
+	}
+		
+		
+	function arrancar (){
+		
+		
+	}
+	
+	function girar (){
+		
+		
+	}
+	
+	function frenar (){
+		
+		
+	}
+	
+	
+}
+
+$renault=new Coche();
+$mazda=new Coche();
+$chevrolet=new Coche();
+
+
+
+?>
